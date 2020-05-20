2e33f05e4a6647db411e9459e8b1448807cea6c1
yedeyao <yedeyao@gnlab.com>
2020-05-20 14:59:43 +0800

    rename folder.

--- build_baidu_keyword.bat
+++ build_baidu_keyword.bat
@@ -1,9 +1,9 @@
 @call makeapp.bat
 
-@call makeskinzip.bat BaiduKeyword
+@call makeskinzip.bat gnlab
 
-".\NSIS\makensis.exe" ".\SetupScripts\BaiduKeyword\baidu_keyword_setup.nsi"
+".\NSIS\makensis.exe" ".\SetupScripts\gnlab\baidu_keyword_setup.nsi"
 
 @rem 如果要调试错误，请使用下面的脚本，这样会打开编译界面（命令行界面中文会显示成?号）
-@rem ".\NSIS\makensisw.exe" ".\SetupScripts\BaiduKeyword\baidu_keyword_setup.nsi"
+@rem ".\NSIS\makensisw.exe" ".\SetupScripts\gnlab\baidu_keyword_setup.nsi"
 @pause
--- build_baidu_keyword_nozip.bat
+++ build_baidu_keyword_nozip.bat
@@ -1,9 +1,9 @@
 @call makensiscode.bat
 
-@call makeskinzip.bat BaiduKeyword
+@call makeskinzip.bat gnlab
 
-".\NSIS\makensis.exe" /DINSTALL_WITH_NO_NSIS7Z=1 ".\SetupScripts\BaiduKeyword\baidu_keyword_setup.nsi"
+".\NSIS\makensis.exe" /DINSTALL_WITH_NO_NSIS7Z=1 ".\SetupScripts\gnlab\baidu_keyword_setup.nsi"
 
 @rem 如果要调试错误，请使用下面的脚本，这样会打开编译界面（命令行界面中文会显示成?号）
-@rem ".\NSIS\makensisw.exe" /DINSTALL_WITH_NO_NSIS7Z=1 ".\SetupScripts\BaiduKeyword\baidu_keyword_setup.nsi"
+@rem ".\NSIS\makensisw.exe" /DINSTALL_WITH_NO_NSIS7Z=1 ".\SetupScripts\gnlab\baidu_keyword_setup.nsi"
 @pause
